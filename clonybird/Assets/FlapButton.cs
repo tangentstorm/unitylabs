faa20569ae47c258530541161cd7d8e87f39360b
tangentstorm <michal.wallace@gmail.com>
2015-03-04 23:35:26 -0600

    import clonybird (work in progress)

---
+++ clonybird/Assets/FlapButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlapButton : MonoBehaviour {
+
+	void OnCollision2D (Collision2D c) {
+		if (c.gameObject.tag == "ceil") {}
+		else {
+			GameLogic.GameOver();
+		}
+	}
+
+}
---
+++ clonybird/Assets/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	static GameLogic self; // singleton
+	Animator animator;
+
+	public void Awake() {
+		if (self == null) {
+			self = this;
+			self.animator = GetComponent<Animator>();
+		} else Destroy(this);
+	}
+
+	public static void GameOver() {
+		self.animator.SetTrigger("GameOver");
+	}
+
+}
